57f8c357e998bddf89e5af952f30b0fe1a8f9b9d
Georgi Yordanov <a12036828@univie.ac.at>
2025-01-09 16:12:24 +0100

    Update of SQL create part. Some VARCHAR attributes need to be longer

--- M1/SQL/IMSE_G2_M1_SQL_CREATE.sql
+++ M1/SQL/IMSE_G2_M1_SQL_CREATE.sql
@@ -1,27 +1,40 @@
-/*  */-- Will use MariaDB
-
-
--- Relations
-
--- @block
 -- pk_save is now a composite pk, member_id in this table is also new
 CREATE TABLE saved (
   member_id INT NOT NULL, 
   tutorial_id INT NOT NULL,
-  CONSTRAINT pk_save PRIMARY KEY (member_id, tutorial_id)
+  PRIMARY KEY (member_id, tutorial_id)
 );
 
+-- @block
+-- auto increment added, attribute order changed to improve readability, attribute types changed
+-- additionally CHECK added to make the difficulty_level more uniform across entries
+CREATE TABLE tutorial (
+  tutorial_id INT AUTO_INCREMENT,
+  name VARCHAR(128) NOT NULL,
+  muscle_group VARCHAR(64),
+  difficulty_level VARCHAR(32) CHECK (difficulty_level IN ('beginner', 'intermediate', 'advanced')),
+  description VARCHAR(2048),
+  PRIMARY KEY (tutorial_id)
+);
+
+-- @block
+-- everything changed
+CREATE TABLE video_tutorial (
+  tutorial_id INT NOT NULL,
+  duration INT,
+  url VARCHAR(2048),
+  PRIMARY KEY (tutorial_id)
+);
 
 -- @block
 -- this table was empty in the auto generated code
 CREATE TABLE recommend (
     recommender_tutorial_id INT NOT NULL,
     recommended_tutorial_id INT NOT NULL,
-    CONSTRAINT pk_recommend PRIMARY KEY (recommender_tutorial_id, recommended_tutorial_id)
+    PRIMARY KEY (recommender_tutorial_id, recommended_tutorial_id),
+    FOREIGN KEY (recommender_tutorial_id) REFERENCES video_tutorial(tutorial_id),
+    FOREIGN KEY (recommended_tutorial_id) REFERENCES video_tutorial(tutorial_id)
 );
-
-
--- Entities
 
 -- @block
 -- auto increment added, attribute order changed to improve readability, attribute types changed
@@ -31,16 +44,16 @@ CREATE TABLE member (
   age INT CHECK(age > 16),
   password VARCHAR(32),
   location_id INT,
-  CONSTRAINT pk_member PRIMARY KEY (member_id)
+  PRIMARY KEY (member_id)
 );
 
 -- @block
 -- pk changed, foreign keys added
 CREATE TABLE bank_details (
   member_id INT NOT NULL, 
-  card_number VARCHAR(19),
+  card_number VARCHAR(32),
   exp_date VARCHAR(32),
-  CONSTRAINT pk_bank_details PRIMARY KEY (member_id),
+  PRIMARY KEY (member_id),
   FOREIGN KEY (member_id) REFERENCES member(member_id) ON DELETE CASCADE
 );
 
@@ -49,29 +62,8 @@ CREATE TABLE bank_details (
 CREATE TABLE location (
   location_id INT AUTO_INCREMENT,
   manager_name VARCHAR(32),
-  address VARCHAR(32),
-  CONSTRAINT pk_location PRIMARY KEY (location_id)
-);
-
--- @block
--- auto increment added, attribute order changed to improve readability, attribute types changed
--- additinally CHECK added to make the difficuty_level more uniform across entries
-CREATE TABLE tutorial (
-  tutorial_id INT AUTO_INCREMENT,
-  name VARCHAR(32) NOT NULL,
-  muscle_group VARCHAR(32),
-  difficulty_level VARCHAR(32) CHECK (difficulty_level IN ('beginner', 'intermediate', 'advanced')),
-  description VARCHAR(2048),
-  CONSTRAINT pk_tutorial PRIMARY KEY (tutorial_id)
-);
-
--- @block
--- everything changed
-CREATE TABLE video_tutorial (
-  tutorial_id INT NOT NULL,
-  duration INT,
-  url VARCHAR(2083),
-  CONSTRAINT pk_video_tutorial PRIMARY KEY (tutorial_id)
+  address VARCHAR(255),
+  PRIMARY KEY (location_id)
 );
 
 
@@ -109,3 +101,11 @@ ALTER TABLE recommend ADD CONSTRAINT fk_recommended_video FOREIGN KEY (recommend
 -- @block
 -- completely new
 ALTER TABLE video_tutorial ADD CONSTRAINT fk_video_tutorial FOREIGN KEY (tutorial_id) REFERENCES tutorial(tutorial_id);
+
+DROP TABLE IF EXISTS bank_details;  
+ DROP TABLE IF EXISTS saved;                               
+ DROP TABLE IF EXISTS member; 
+ DROP TABLE IF EXISTS location;                    
+ DROP TABLE IF EXISTS recommend;                     
+ DROP TABLE IF EXISTS video_tutorial;                     
+ DROP TABLE IF EXISTS tutorial;                     
